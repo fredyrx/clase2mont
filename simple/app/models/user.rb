184039f6ef7af57d0615fa1cd7058ce412289e44
fredyrx <fredy.rx@gmail.com>
2015-09-10 02:59:32 +0000

    add bootstrap

--- simple/app/models/user.rb
+++ simple/app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :integer          not null, primary key
+#  username    :string(255)
+#  password    :string(255)
+#  email       :string(255)
+#  remember_me :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#
+
 class User < ActiveRecord::Base
+    belongs_to :category
 end
--- simple/test/models/user_test.rb
+++ simple/test/models/user_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :integer          not null, primary key
+#  username    :string(255)
+#  password    :string(255)
+#  email       :string(255)
+#  remember_me :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
